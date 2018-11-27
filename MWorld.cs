40097e8d9a1d5d411a6ea9dfaf9b9fb4a6437be6
Vex <fmcx1000@gmail.com>
2018-11-27 14:26:26 -0600

    Update MWorld.cs

--- MWorld.cs
+++ MWorld.cs
@@ -21,4 +21,29 @@ namespace HavenMod
         public static bool spawnOre = false;
 
     }
+    public override void PostWorldGen()
+		{
+		
+
+			
+			int[] itemsToPlaceInChests = new int[] { mod.ItemType("saltcrystal"), mod.ItemType("potassiumchunk") };
+			int itemsToPlaceInChestsChoice = 50;
+			for (int chestIndex = 0; chestIndex < 1000; chestIndex++)
+			{
+				Chest chest = Main.chest[chestIndex];
+				
+				if (chest != null && Main.tile[chest.x, chest.y].type == TileID.Containers && Main.tile[chest.x, chest.y].frameX == 0 * 36)
+				{
+					for (int inventoryIndex = 0; inventoryIndex < 40; inventoryIndex++)
+					{
+						if (chest.item[inventoryIndex].type == 0)
+						{
+							chest.item[inventoryIndex].SetDefaults(itemsToPlaceInChests[itemsToPlaceInChestsChoice]);
+							itemsToPlaceInChestsChoice = (itemsToPlaceInChestsChoice + 1) % itemsToPlaceInChests.Length;
+							
+						}
+					}
+				}
+			}
+		}
 }
