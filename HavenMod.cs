e4d62a00bec3e423f4f2f99c049174d526e20780
CooL <36611031+colin1S@users.noreply.github.com>
2018-11-28 22:17:16 -0500

    Update HavenMod.cs

--- HavenMod.cs
+++ HavenMod.cs
@@ -4,8 +4,23 @@ namespace HavenMod
 {
 	class HavenMod : Mod
 	{
-		public HavenMod()
-		{
-		}
+	public HavenMod()
+        {
+            Properties = new ModProperties()
+            {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
+        }
+
+        //Setting music for Invasion
+        public override void UpdateMusic(ref int music)
+        {
+            if (MWorld.dungueonInvasionUp && (Main.invasionX == (double)Main.spawnTileX))
+            {
+                music = GetSoundSlot(SoundType.Music, "Sounds/Music/NeoWestern");
+            }
+        }
 	}
 }
