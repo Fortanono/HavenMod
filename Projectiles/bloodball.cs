5e2626ed66f11b3d265c03ea5068dbf3fd6c1899
Vex <fmcx1000@gmail.com>
2018-12-13 17:04:46 -0600

    Add files via upload

---
+++ Projectiles/bloodball.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace HavenMod.Projectiles
+{
+	public class bloodball : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Blood Ball");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.CloneDefaults(ProjectileID.EyeBeam);
+			aiType = ProjectileID.EyeBeam;
+			projectile.friendly = false;
+		}
+		public override void AI()
+		{
+		int dustnumber = Dust.NewDust(projectile.position, projectile.width, projectile.height, 5, 0f, 0f, 200, default(Color), 3f);
+		}
+
+	}
+}
