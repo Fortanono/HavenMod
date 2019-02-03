e42cf54c842820bfb6429ecbaf3003eff9ae69bb
CooL <36611031+colin1S@users.noreply.github.com>
2019-02-02 23:24:25 -0500

    bullets come out of muzzle correctly etc.

--- Items/Weapons/Ranged/minicoyote.cs
+++ Items/Weapons/Ranged/minicoyote.cs
@@ -38,12 +38,23 @@ namespace HavenMod.Items.Weapons.Ranged
 			Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(15));
 			speedX = perturbedSpeed.X;
 			speedY = perturbedSpeed.Y;
+
+			Vector2 muzzleOffset = Vector2.Normalize(new Vector2(speedX, speedY)) * 25f;
+			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+			{
+				position += muzzleOffset;
+			}
 			return true;
 		}
 
 		public override bool ConsumeAmmo(Player player)
 		{
 			return Main.rand.NextFloat() >= .5f;
+		}
+
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(0, 0);
 		}
 	}
 }
